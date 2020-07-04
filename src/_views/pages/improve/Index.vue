13475967e7bf39a31608ffd3e2986f54053242f2
Usama Ayaz <usama.ayaz@live.com>
2020-07-04 12:16:12 +0500

    Remove Old BackUp Files

--- src/_views/pages/improve/Index.vue
+++
@@ -1,4 +0,0 @@
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('default-layout',[_c('router-view')],1)}
-var staticRenderFns = []
-
-export { render, staticRenderFns }
--- src/_views/pages/improve/tool_ideas/CentralBar.vue
+++
@@ -1,4 +0,0 @@
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('process-selector',{attrs:{"max-level":"phase","show-count":"toolIdeas","section":"toolIdeas","list":{withStages:true},"detail":{withStagesFull:true}}})],1)}
-var staticRenderFns = []
-
-export { render, staticRenderFns }
